1ea3a087dcee5647a25ee7a91e0fd064d9d2d8be
Caio Vellani <caiovellani@outlook.com>
2025-03-19 17:55:42 -0300

    feat: Add user creation route
    - Remove test route
    - Import CreateUserController
    - Add POST /users route to handle user creation

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,13 @@
 import { Router, Request, Response } from 'express'
+import { CreateUserController } from './controllers/user/CreateUserController'
 
 const router = Router()
 
-router.get('/teste', (req: Request, res: Response) => {
-  res.json({ ok: true })
-})
+// router.get('/teste', (req: Request, res: Response) => {
+//   res.json({ ok: true })
+// })
+
+// -- USER ROUTES --
+router.post('/users', new CreateUserController().handle)
 
 export { router }
