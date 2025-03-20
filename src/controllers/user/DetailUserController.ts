522e74dabe6c4319f1043e7947606826b5727c3b
Caio Vellani <caiovellani@outlook.com>
2025-03-20 16:46:22 -0300

    Implement user details functionality
    - Remove console.log from DetailUserController
    - Pass user_id to DetailUserService.execute()
    - Implement DetailUserService to fetch user details from database
    - Include user's subscription information in the response

--- src/controllers/user/DetailUserController.ts
+++ src/controllers/user/DetailUserController.ts
@@ -5,11 +5,10 @@ import { DetailUserService } from '../../services/user/DetailUserService'
 class DetailUserController {
   async handle(request: Request, response: Response) {
     const user_id = request.user_id
-    console.log(user_id)
 
     const detailUserSerice = new DetailUserService()
 
-    const detailUser = await detailUserSerice.execute()
+    const detailUser = await detailUserSerice.execute(user_id)
 
     return response.json(detailUser)
   }
